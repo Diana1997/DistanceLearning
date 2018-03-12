6d6ec36b55bc7403f767f54797bba91d34b3f79a
Diana <Diana@DESKTOP-3FSOTT0>
2018-03-12 17:24:29 +0400

    initial commit

---
+++ DistanceLearning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DistanceLearning.Startup))]
+namespace DistanceLearning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
